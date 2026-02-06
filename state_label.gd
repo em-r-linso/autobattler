e0351c0e56063499e0369ac21d316853aecb6244
Emma Linso <em@linso.online>
2026-02-06 00:07:58 -0700

    Add Ridiculous Coding plugin lmao

--- state_label.gd
+++ state_label.gd
@@ -7,7 +7,7 @@ func _enter_tree():
 
 func _exit_tree():
 	SignalBus.game_state_changed.disconnect(on_game_state_changed)
-	
+
 	
 func on_game_state_changed(state : int, state_name : String):
 	print("on game state changed")
